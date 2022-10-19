b74a08091b25883032a69c1d689ed3d86ad2296b
Vinita Vaswani <vinitavaswani24@gmail.com>
2022-10-20 01:40:45 +0530

    view-appts

--- validation.php
+++ validation.php
@@ -6,11 +6,22 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $sql = "select * from login where email='$email' && password='$password'";
 $result = mysqli_query($con, $sql);
-$num = mysqli_num_rows($result);
-if ($num == 1) {
+$name = '';
+if ($num = mysqli_num_rows($result)) {
+    while ($row = mysqli_fetch_array($result)) {
+        $name = $row['username'];
+    }
     $_SESSION['uname'] = $name;
-    header('location:home.php');
-} else {
+    $_SESSION['email'] = $email;
+    // header('location:dashboard.php');
+    header('location:patient.php');
+}
+// $num = mysqli_num_rows($result);
+// if ($num == 1) {
+//     $_SESSION['uname'] = $num['username'];
+//     header('location:home.php');
+// }
+else {
     echo '<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script type="text/javascript">
